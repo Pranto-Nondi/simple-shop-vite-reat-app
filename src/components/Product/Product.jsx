88f0721454037492e74991b75c3261f7a70971f6
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-03-28 00:29:09 +0600

    update on work

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 
 const Product = (props) => {
     // console.log(props)
-    const { img, name, price, ratings, seller } = props.product
+    const {img, name, price, ratings, seller } = props.product
+    const addToCard=props.addToCard;
     return (
         <div>
             <div className="card w-80 ml-7 h-full bg-base-100 shadow-2xl">
-                <figure className='w-full'><img className='w-[90%] rounded md:rounded-md lg:rounded-lg' src={img} alt="Shoes" />
+                <figure className='w-full'><img className='w-[90%] rounded ' src={img} alt="Shoes" />
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title">{name}</h2>
                     <p>Price: {price}</p>
                     <small>Manufacturer : {seller}</small>
                     <small>Ratings : {ratings} star</small>
-                    <div className=' bg-amber-200 p-5 rounded md:rounded-md lg:rounded-lg'>
-                        <h1 className='text-black'>Add To Cart</h1>
+                    <div className=' bg-amber-200 hover:bg-amber-300 p-5 rounded '>
+                        <h1 onClick={()=>addToCard(props.product)} className='text-black'>Add To Cart</h1>
                     </div>
                 </div>
             </div>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,6 +3,10 @@ import Product from '../Product/Product';
 import './Shop.css'
 const Shop = () => {
     const [products, setProducts] = useState([])
+    const [card, setCard] = useState([]);
+    const addToCard = (product) => {
+        setCard([...card, product])
+    }
     useEffect(() => {
         fetch(`products.json`)
             .then(res => res.json())
@@ -14,17 +18,19 @@ const Shop = () => {
             <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3  
             w-full gap-3 md:w-[70%] lg:w-[70%] pt-10 mx-auto'>
                 {
-                    products.map(product => <Product product={product} key={product.id} />)
+                    products.map(product => <Product product={product}
+                        addToCard={addToCard} key={product.id} />)
                 }
 
             </div>
             <div className=' w-full md:w-[30%] lg:[30%] pt-10  mx-auto'>
-              
-                <div className="card w-80 ml-7 bg-amber-400 text-primary-content">
+
+                <div className="card w-80 ml-7 bg-amber-400 text-primary-content ">
                     <div className="card-body">
-                        <h2 className="card-title">Order Summary:</h2>
-                        
-                        
+                        <h2 className="card-title">Order Summary</h2>
+                        <p>Selected Items: {card.length}</p>
+
+
                     </div>
                 </div>
             </div>
